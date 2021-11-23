4bbb83ce028c1687f30464d9b76e17d6adc620f1
RIno256 <komyshan.sergei@gmail.com>
2021-11-23 21:30:11 +0600

    1

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IP address information</title>
+</head>
+<body>
+    <div class=table>
+        <table>
+            <tr> 
+                <td>
+                    <h1>User agent: Raw</h1>
+                <td>
+                <td>
+                    <?php
+                        include("php/browser_info.php");
+                    ?>
+                <td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
