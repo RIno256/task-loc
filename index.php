ffc17bef3ac85f84860b55f16fc65d6b10fd1261
RIno256 <komyshan.sergei@gmail.com>
2021-11-25 03:18:52 +0600

    Create task

--- index.php
+++ index.php
@@ -15,17 +15,33 @@
         #################################################
         $read_city = new Reader('/var/www/task.test/db/GeoLite2-City.mmdb');
         $read_country = new Reader('/var/www/task.test/db/GeoLite2-Country.mmdb');
-        $read_asn = new Reader('/var/www/task.test/db/GeoLite2-ASN.mmdb');
         #################################################
         #$ASN_=
         $City =  $read_city -> city($real_ip);
-        #$Country_= $read_country -> country($read_ip);
-        $isso= $c->country->isoCode;
     ?>
     <div class= "content">
         <div class= "head_ip">
             <p><h1>Your IP is: </h1><p>
             <p><code><h3><?php echo $real_ip ?></h3></code></p>
+            </br>
+            <p><h2>Information</h2></p>
+            <?php
+                 print($record->country->isoCode . "\n"); // 'US'
+                 print($record->country->name . "\n"); // 'United States'
+                 print($record->country->names['zh-CN'] . "\n"); // '美国'
+         
+                 print($record->mostSpecificSubdivision->name . "\n"); // 'Minnesota'
+                 print($record->mostSpecificSubdivision->isoCode . "\n"); // 'MN'
+         
+                 print($record->city->name . "\n"); // 'Minneapolis'
+         
+                 print($record->postal->code . "\n"); // '55455'
+         
+                 print($record->location->latitude . "\n"); // 44.9733
+                 print($record->location->longitude . "\n"); // -93.2323
+         
+                 print($record->traits->network . "\n"); // '128.101.101.101/32'
+            ?>
         <di>
     </div>
 </body>
